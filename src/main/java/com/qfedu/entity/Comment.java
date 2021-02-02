f562abf143951c319416b58fcd2bb94d1a0b369f
zhangmenghan <1091969221@qq.com>
2021-02-02 19:34:04 +0800

    Initial commit

---
+++ src/main/java/com/qfedu/entity/Comment.java
@@ -0,0 +1,51 @@
+package com.qfedu.entity;
+
+import java.sql.Timestamp;
+
+public class Comment {
+    private Integer pid;
+    private String pcontent;
+    private Integer userid;
+    private Integer byid;
+    private Timestamp   time;
+
+    public Integer getPid() {
+        return pid;
+    }
+
+    public void setPid(Integer pid) {
+        this.pid = pid;
+    }
+
+    public String getPcontent() {
+        return pcontent;
+    }
+
+    public void setPcontent(String pcontent) {
+        this.pcontent = pcontent;
+    }
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public Integer getByid() {
+        return byid;
+    }
+
+    public void setByid(Integer byid) {
+        this.byid = byid;
+    }
+
+    public Timestamp getTime() {
+        return time;
+    }
+
+    public void setTime(Timestamp time) {
+        this.time = time;
+    }
+}
---
+++ src/main/java/com/qfedu/entity/Picture.java
@@ -0,0 +1,22 @@
+package com.qfedu.entity;
+
+public class Picture {
+    private Integer userid;
+    private String photo;
+
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
