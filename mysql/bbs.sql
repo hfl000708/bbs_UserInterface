21e9c8426ec0589d5a99b2197ec874527481084b
zhangmenghan <1091969221@qq.com>
2021-02-02 19:41:36 +0800

    mysql 数据库表的sql文件

---
+++ mysql/bbs.sql
@@ -0,0 +1,88 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost_3306
+Source Server Version : 50729
+Source Host           : localhost:3306
+Source Database       : bbs
+
+Target Server Type    : MYSQL
+Target Server Version : 50729
+File Encoding         : 65001
+
+Date: 2021-02-02 19:40:29
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `pid` int(11) NOT NULL AUTO_INCREMENT COMMENT '评论',
+  `pcontent` varchar(400) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `byid` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`pid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of comment
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for picture
+-- ----------------------------
+DROP TABLE IF EXISTS `picture`;
+CREATE TABLE `picture` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `photo` varchar(255) NOT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of picture
+-- ----------------------------
+INSERT INTO `picture` VALUES ('6', '2018_10_26_10_51_35_id6_156e36861bcbe7ccd83b9bada2e99322.jpg');
+
+-- ----------------------------
+-- Table structure for post
+-- ----------------------------
+DROP TABLE IF EXISTS `post`;
+CREATE TABLE `post` (
+  `fid` int(11) NOT NULL AUTO_INCREMENT,
+  `titles` varchar(100) NOT NULL,
+  `fcontent` varchar(2000) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `photo` varchar(200) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `username` varchar(45) NOT NULL,
+  PRIMARY KEY (`fid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COMMENT='发帖表';
+
+-- ----------------------------
+-- Records of post
+-- ----------------------------
+INSERT INTO `post` VALUES ('4', '我是陈意涵', '大家好，我爱你们。', '6', '2018_10_26_10_51_57_id6_2017_9_1_16_57_31_id7_814f253234a906a1d422f3c4fe832fa1.jpg', '2018-10-26 10:51:57', '陈意涵');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `family` varchar(45) DEFAULT NULL,
+  `intro` varchar(255) DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('6', '陈意涵', '123456', '924818949@qq.com', '成都', '长得漂亮。', '2017-12-18 19:44:57');
